59fc860ef2086c74ed4501466fbe68de64d1fe2e
Jogibeda <117044716+Jogibeda@users.noreply.github.com>
2023-10-13 10:41:42 -0700

    creacionMenu

---
+++ Python/Ejercicio A/app.py
@@ -0,0 +1,22 @@
+import csv
+
+print("Por favor dime como quieres mostrar el listado")
+print("1. Por clase")
+print("2. Por alguna caracteristicas")
+print("Por favor proporciona el numero de tu eleccion")
+Seleccion=input()
+
+if int(Seleccion)==1:
+    pass
+elif int(Seleccion)==2:
+    pass
+else:
+    print("Selecciona una opcion de las antes mencionadas")
+
+
+class Clase():
+    pass
+
+
+class Caracteristica():
+    pass
