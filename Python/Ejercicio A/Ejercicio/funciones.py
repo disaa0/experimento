0f911a5437284a4da537d958f80d64839321c04c
AkiroMiami <123915508+AkiroMiami@users.noreply.github.com>
2023-10-13 11:32:50 -0700

    987654_sin_asistencia

---
+++ Python/Ejercicio A/Ejercicio/funciones.py
@@ -0,0 +1,33 @@
+import csv
+from csv import DictWriter
+
+def mostrar_datos_clases(desde_archivo):
+    datos = {}
+    with open(desde_archivo, mode='r', newline='', encoding='utf-8') as archivo:
+        lector_csv = csv.DictReader(archivo)
+        for fila in lector_csv:
+            fila['Clase_id'] = int(fila['Clase_id'])
+            datos[fila['Clase_id']] = str(fila['Clase_tipo'])
+    return datos
+
+def mostrar_datos_zoo(desde_archivo):
+    datos = {}
+    with open(desde_archivo, mode='r', newline='', encoding='utf-8') as archivo:
+        lector_csv = csv.DictReader(archivo)
+        for fila in lector_csv:
+            datos[fila['nombre_animal']] = fila
+    return datos
+
+def listar_por_clase(datos, clase):
+    resultado = []
+    for animal in datos.values():
+        if int(animal['clase']) == clase:
+            resultado.append(animal)
+    return resultado
+
+def listar_por_caracteristica(datos, caracteristica):
+    resultado = []
+    for animal in datos.values():
+        if int(animal[caracteristica]) == 1:
+            resultado.append(animal)
+    return resultado
